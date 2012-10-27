b9540a480b4bb8632cb9e06288e5d16e86e73450
Tony Ju <tju@mac.local>
2012-10-28 01:37:36 +0800

    main

--- Projects/Python/stock/main.py
+++ Projects/Python/stock/main.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
+from env import app_config
+from utils.app_config import AppConfig
 from handler.trans_data_handler import TransDataHandler
+from handler.stock_handler import StockHandler
+from handler.profile_handler import ProfileHandler
+
+class Main:
+	def run(self):
+		nodes = app_config.get('app', 'nodes').split(',')
+		actions = app_config.get('app', 'actions').split(',')
+		m = 0
+		for node in nodes:
+			if m==0:
+				config_file = 'config/' + node + '.ini'
+				config = AppConfig().load(config_file)
+				action = actions[m]+'(config)'
+				handler = eval(action)
+				handler.run()
+				m = m + 1
 
 if __name__ == '__main__':
-	handler = TransDataHandler()
-	handler.run()
+	main = Main()
+	main.run()
